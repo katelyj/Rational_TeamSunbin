512041caa5817480b7f14f410c867a25724868b9
Kate Ly <kjohnston@stuy.edu>
2015-11-17 18:54:29 -0500

    Update Rational.java

--- Rational.java
+++ Rational.java
@@ -1,1 +1,41 @@
+public class Rational {
 
+    private int numerator;
+    private int denominator;
+
+    public Rational() {
+	numerator = 0;
+	denominator = 1;
+    }
+
+    public Rational(int n, int d) {
+	if ( d == 0 ) {
+	    System.out.println("Nope.");
+	    numerator = 0;
+	    denominator = 1;
+	}
+	else {
+	    numerator = n;
+	    denominator = d;
+	}
+    }
+
+    public String toString() {
+	return numerator + " / " + denominator;
+    }
+
+    public double floatValue() {
+	return numerator/(double)denominator;
+    }
+
+    public void multiply(Rational s) {
+	this.numerator *= s.numerator;
+	this.denominator *= s.denominator;
+    }
+
+    public void divide(Rational s) {
+	this.numerator /= s.numerator;
+	this.denominator /= s.denominator;
+    }
+
+}
